f947761020b9478813cb669f59468ad5b8c20646
VANSH PRATAP SINGH <vanshpratapsingh1121@gmail.com>
2024-12-20 02:36:25 +0530

    change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import connectDB from "./db/index.js";
 
 //load enviornment variables
 dotenv.config({
-    path: './env',
+    path: './.env',
 });
 
 //connect to database define in db folder in index.js
